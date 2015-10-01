3f709bbbf60021ac02e7f447b92f4a61d197d59e
Pakkapon Phongthawee <me@pureapp.in.th>
2015-10-01 21:47:07 +0700

    WIP : fb-app

--- assets/js/fb-loader.js
+++ assets/js/fb-loader.js
@@ -4,4 +4,12 @@
   js = d.createElement(s); js.id = id;
   js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.4&appId=351070565012663";
   fjs.parentNode.insertBefore(js, fjs);
+  $(document).trigger('fbInit');
+  var FBlogin_master = function(){
+    FB.login(function(response) {
+      if (response.status === 'connected') {
+        $(document).trigger('fbLogin');
+      }
+    });
+  }
 }(document, 'script', 'facebook-jssdk'));
